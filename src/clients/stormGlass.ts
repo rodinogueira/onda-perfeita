44009485d80543e0d2a0cb1c31d5ab73db9e4642
Rodrigo Nogueira <rdg6design@gmail.com>
2023-05-11 11:46:57 -0300

    Normalizando a resposta da API

--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -1,16 +1,73 @@
 import { AxiosStatic } from 'axios';
 
+export interface StormGlassPointSource {
+    [key: string]: number;
+}
+
+export interface StormGlassPoint {
+    readonly time: string;
+    readonly swellDirection: StormGlassPointSource;
+    readonly swellHeight: StormGlassPointSource;
+    readonly swellPeriod: StormGlassPointSource;
+    readonly waveHeight: StormGlassPointSource;
+    readonly waveDirection: StormGlassPointSource;
+    readonly windDirection: StormGlassPointSource;
+    readonly windSpeed: StormGlassPointSource;
+}
+
+export interface StormGlassForecastResponse {
+    hours: StormGlassPoint[];
+}
+
+export interface ForecastPoint {
+    time: string;
+    swellDirection: number;
+    swellHeight: number;
+    swellPeriod: number;
+    waveHeight: number;
+    waveDirection: number;
+    windDirection: number;
+    windSpeed: number;
+}
+
 export class StormGlass {
     readonly stormGlassAPIParams = 'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed';
     readonly stormGlassAPISource = 'noaa';
-
     constructor(protected request: AxiosStatic) {}
-// 360cd8f2-ee87-11ed-86b2-0242ac130002-360cd956-ee87-11ed-86b2-0242ac130002
-    public async fetchPoints(lat: number, lng: number): Promise<{}> {
-        // https://api.stormglass.io/v2/weather/point?params=swellDirection%2CswellHeight%2CswellPeriod%2CwaveDirection%2CwaveHeight%2CwindDirection%2CwindSpeed&source=noaa&lat=58.7984&lng=17.8081
-        // this.request.get(`https://api.stormglass.io/v2/weather/point?params=${this.stormGlassParams}&source=noaa&lat=${lat}&lng=${lng}`);
+    
+    public async fetchPoints(lat: number, lng: number): Promise<ForecastPoint[]> {
+        
+        const response = await this.request.get<StormGlassForecastResponse>(
+            `https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}`
+            );
+        return this.normalizeResponse(response.data);
+    }
 
-        this.request.get(`https://api.stormglass.io/v2/weather/point?params=swellDirection%2CswellHeight%2CswellPeriod%2CwaveDirection%2CwaveHeight%2CwindDirection%2CwindSpeed&source=noaa&lat=58.7984&lng=17.8081`);
-        return Promise.resolve({});        
+    private normalizeResponse(
+        points: StormGlassForecastResponse
+    ): ForecastPoint[] {
+        return points.hours.filter(this.isValidPoint.bind(this)).map((point) => ({
+            swellDirection: point.swellDirection[this.stormGlassAPISource],
+            swellHeight: point.swellHeight[this.stormGlassAPISource],
+            swellPeriod: point.swellPeriod[this.stormGlassAPISource],
+            time: point.time,
+            waveDirection: point.waveDirection[this.stormGlassAPISource],
+            waveHeight: point.waveHeight[this.stormGlassAPISource],
+            windDirection: point.windDirection[this.stormGlassAPISource],
+            windSpeed: point.windSpeed[this.stormGlassAPISource],
+          }));
+    }
+
+    private isValidPoint(point: Partial<StormGlassPoint>): boolean {
+        return !!(
+            point.time &&
+            point.swellDirection?.[this.stormGlassAPISource] &&
+            point.swellHeight?.[this.stormGlassAPISource] &&
+            point.swellPeriod?.[this.stormGlassAPISource] &&
+            point.waveHeight?.[this.stormGlassAPISource] &&
+            point.waveDirection?.[this.stormGlassAPISource] &&
+            point.windDirection?.[this.stormGlassAPISource] &&
+            point.windSpeed?.[this.stormGlassAPISource]
+        );
     }
 }
