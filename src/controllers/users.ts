2130e7df71dcdc669778247b9de735573495bab1
Rodrigo Nogueira <rdg6design@gmail.com>
2023-09-17 06:05:47 -0300

    Módulo de users - Mongoose hooks, validation e Type narrowing

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,13 +1,18 @@
-import { Controller, Post } from "@overnightjs/core";
+import { Controller, Post } from '@overnightjs/core';
 import { Response, Request } from 'express';
 import { User } from '@src/models/user';
+import { BaseController } from './index';
 
 @Controller('users')
-export class UsersController {
-    @Post('')
-    public async create(req: Request, res: Response): Promise <void> {
-        const user = new User(req.body);
-        const newUser = await user.save();
-        res.status(201).send(newUser);
+export class UsersController extends BaseController {
+  @Post('')
+  public async create(req: Request, res: Response): Promise<void> {
+    try {
+      const user = new User(req.body);
+      const newUser = await user.save();
+      res.status(201).send(newUser);
+    } catch (error) {
+      this.sendCreateUpdateErrorResponse(res, error);
     }
+  }
 }
--- src/models/user.ts
+++ src/models/user.ts
@@ -1,4 +1,4 @@
-import mongoose, { Document, Model, Types } from 'mongoose';
+import mongoose, { Document, Model } from 'mongoose';
 
 export interface User {
   _id?: string;
@@ -7,9 +7,11 @@ export interface User {
   password: string;
 }
 
-interface UserModel extends Omit<User, '_id'>, Document {
-  _id: Types.ObjectId;
+export enum CUSTOM_VALIDATION {
+  DUPLICATED = 'DUPLICATED',
 }
+
+interface UserModel extends Omit<User, '_id'>, Document {}
 
 const schema = new mongoose.Schema(
   {
@@ -17,19 +19,29 @@ const schema = new mongoose.Schema(
     email: {
       type: String,
       required: true,
-      unique: true,
     },
     password: { type: String, required: true },
   },
   {
     toJSON: {
       transform: (_, ret): void => {
-        ret.id = ret._id.toString();
+        ret.id = ret._id;
         delete ret._id;
         delete ret.__v;
       },
     },
   }
+);
+/**
+ * Validates the email and throws a validation error, otherwise it will throw a 500
+ */
+schema.path('email').validate(
+  async (email: string) => {
+    const emailCount = await mongoose.models.User.countDocuments({ email });
+    return !emailCount;
+  },
+  'already exists in the database.',
+  CUSTOM_VALIDATION.DUPLICATED
 );
 
 export const User: Model<UserModel> = mongoose.model<UserModel>('User', schema);
