ae086f3970ee8c8d91835ec36a48b10a1ee96229
Rodrigo Nogueira <rdg6design@gmail.com>
2023-11-09 09:33:46 -0300

    test /me

--- test/functional/users.test.ts
+++ test/functional/users.test.ts
@@ -20,8 +20,9 @@ describe('Users functional tests', () => {
       expect(response.body).toEqual(
         expect.objectContaining({
           ...newUser,
-          ...{ password: expect.any(String) }
-        }));
+          ...{ password: expect.any(String) },
+        })
+      );
     });
 
     it('Should return 422 when there is a validation error', async () => {
@@ -52,29 +53,28 @@ describe('Users functional tests', () => {
       expect(response.body).toEqual({
         code: 409,
         error: 'Conflict',
-        message: 'User validation failed: email: already exists in the database.',
+        message:
+          'User validation failed: email: already exists in the database.',
       });
     });
   });
 
-  describe('When authenticating a user', () => {
-    it('should generate a token for a valed user', async () => {
+  describe('when authenticating a user', () => {
+    it('should generate a token for a valid user', async () => {
       const newUser = {
         name: 'John Doe',
-        email: 'john@gmail.com',
-        password:'1234',
+        email: 'john@mail.com',
+        password: '1234',
       };
-
       await new User(newUser).save();
       const response = await global.testRequest
         .post('/users/authenticate')
         .send({ email: newUser.email, password: newUser.password });
 
-        expect(response.body).toEqual(
-          expect.objectContaining({ token: expect.any(String) })
-        );
-    })
-
+      expect(response.body).toEqual(
+        expect.objectContaining({ token: expect.any(String) })
+      );
+    });
     it('Should return UNAUTHORIZED if the user with the given email is not found', async () => {
       const response = await global.testRequest
         .post('/users/authenticate')
@@ -96,6 +96,40 @@ describe('Users functional tests', () => {
 
       expect(response.status).toBe(401);
     });
-  })
+  });
 
+  describe('When getting user profile info', () => {
+    it(`Should return the token's owner profile information`, async () => {
+      const newUser = {
+        name: 'John Doe',
+        email: 'john@mail.com',
+        password: '1234',
+      };
+      const user = await new User(newUser).save();
+      const token = AuthService.generateToken(user.toJSON());
+      const { body, status } = await global.testRequest
+        .get('/users/me')
+        .set({ 'x-access-token': token });
+
+      expect(status).toBe(200);
+      expect(body).toMatchObject(JSON.parse(JSON.stringify({ user })));
+    });
+
+    it(`Should return Not Found, when the user is not found`, async () => {
+      const newUser = {
+        name: 'John Doe',
+        email: 'john@mail.com',
+        password: '1234',
+      };
+      //create a new user but don't save it
+      const user = new User(newUser);
+      const token = AuthService.generateToken(user.toJSON());
+      const { body, status } = await global.testRequest
+        .get('/users/me')
+        .set({ 'x-access-token': token });
+
+      expect(status).toBe(404);
+      expect(body.message).toBe('User not found!');
+    });
+  });
 });
